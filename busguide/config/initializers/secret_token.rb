360d5c887bc546fc759cf564d9f3afc1536a88a3
iamaun <programerhc@hotmail.com>
2014-03-07 16:03:48 +0700

    first commit

---
+++ busguide/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Busguide::Application.config.secret_key_base = '6ea226fef65d77d5f4b5d8b2be4ac8fb70e00b076e9155f14a2fe518803ad5f704d3ea6e41123dd6bbfd36ec8031406bf35df4d806fb0f06cb574c9dc9b16fe8'
